0662b84f2fc6b00fd813968ad0ac85ce0fd4eeb8
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-22 13:48:11 -0700

    Added semver-regex benchmark tests

---
+++ test/benchmark.test.ts
@@ -0,0 +1,19 @@
+import valid from '../src';
+
+describe('semver-regex benchmarks', () => {
+  it('catatrophic backtracking', () => {
+    const postfix =
+      '.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aa.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'.repeat(
+        99999
+      );
+    expect(valid(`v1.1.3-0aa${postfix}$`)).toBe(false);
+  });
+
+  it('ReDoS vulnerability', () => {
+    const start = Date.now();
+    const fixture = `0.0.0-0${'.-------'.repeat(50000)}@`;
+    valid(fixture);
+    const difference = Date.now() - start;
+    expect(difference).toBeLessThan(10);
+  });
+});
