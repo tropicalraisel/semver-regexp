3b306d101e12ebc89a909c9586d079ec865a299b
TropicalRaisel <tshuler@nationalraisin.com>
2021-09-22 15:07:51 -0700

    Deduped one regex section

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ This is the officially recommended semver 2.0.0 regex that passes all the given 
 All '\d' occurrences from the original have been replaced with '[0-9]'.
 This version actually performs faster than the original.
 */
-
 const regex =
-  /^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)(?:-((?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
+  /^((0|[1-9][0-9]*)\.){2}(0|[1-9][0-9]*)(?:-((?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9][0-9]*|[0-9]*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
 
 export function isValidSemanticVersion(package_version: string): boolean {
   const match = package_version.match(regex);
